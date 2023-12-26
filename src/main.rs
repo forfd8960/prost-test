a790728fc1d3e8cd16aa4170d93d4b41b2da17fd
forfd8960 <forfd8960@gmail.com>
2023-12-26 15:39:32 +0800

    Read and Parse Protobuf Message

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
 mod pb;
 
+use anyhow::Result;
+use bytes::Bytes;
+use prost::Message;
+use tokio::net::TcpListener;
+
 use crate::pb::pb as mypb;
 
-fn main() {
-    let kv = mypb::Kv {
-        key: "Hello".to_string(),
-        value: "World!".to_string(),
-    };
+#[tokio::main]
+async fn main() -> Result<()> {
+    let addr = "127.0.0.1:8989";
+    let listener = TcpListener::bind(addr).await?;
+    println!("start listen on: {}", addr);
 
-    let kv1 = mypb::Kv {
-        key: "Hello".to_string(),
-        value: "World!".to_string(),
-    };
-    println!("{:?}", kv);
-    println!("kv == kv1: {:?}", kv == kv1);
+    loop {
+        let (tcp_stream, addr) = listener.accept().await?;
+        println!("client: {} connected", addr);
+
+        tokio::spawn(async move {
+            let _ = tcp_stream.readable().await;
+
+            let mut buf = [0; 4096];
+            match tcp_stream.try_read(&mut buf) {
+                Ok(0) => {}
+                Ok(n) => {
+                    println!("read {} bytes", n);
+                    let data = Bytes::copy_from_slice(&buf[0..n]);
+                    let req_data = mypb::CommondRequest::decode(data);
+                    match req_data {
+                        Ok(req) => {
+                            println!("decode cmd: {:?}", req);
+                        }
+                        Err(e) => {
+                            println!("failed to decode request: {}", e);
+                        }
+                    }
+                }
+                Err(ref e) => {
+                    println!("read data err: {}", e);
+                }
+            }
+
+            println!("client disconnected: {}", addr);
+        });
+    }
 }
